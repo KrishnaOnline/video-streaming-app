47eb3390ff7528d58365487e69db2e447f9cee82
KrishnaOnline <krishnavamshikusuma@gmail.com>
2023-09-02 12:32:53 +0530

    home page done

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import VideoCard from './VideoCard';
+import { YOUTUBE_VIDEO_URL } from '../utils/constants';
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchApiData();
+  }, []);
+
+  async function fetchApiData() {
+    const data = await fetch(YOUTUBE_VIDEO_URL);
+    const json = await data.json();
+    console.log(json.items);
+    setVideos(json.items);
+  }
+
   return (
-    <div>
-      VideoContainer
+    <div className='flex flex-wrap ml-3'>
+      {
+        videos.map((video) => {
+          return  (
+            <VideoCard video={video} key={video.id}/>
+          ) 
+        })
+      }
     </div>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 const YOUTUBE_API_KEY = "AIzaSyDkg1YC_Cuc7ZYZ_xFOIVnuM5J-dxwN18c";
 
-export const YOUTUBE_VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+YOUTUBE_API_KEY;
+export const YOUTUBE_VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+YOUTUBE_API_KEY;
